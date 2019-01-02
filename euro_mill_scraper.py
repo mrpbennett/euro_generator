6f368ea6df04018a259a927e652520175f313912
Paul Bennett <pbennett.uk@outlook.com>
2019-01-02 17:26:03 +0000

    managed to pull all numbers but still not able to clean the text up.

--- euro_mill_scraper.py
+++ euro_mill_scraper.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
 
 src = requests.get('https://www.national-lottery.co.uk/results/euromillions/draw-history').text
 
 soup = BeautifulSoup(src, 'lxml')
 
-ball_numbers = soup.find('li', class_='table_cell_block')
-print(ball_numbers)
+for results in soup.find_all('li', attrs={'class': 'table_cell_3'}):
+    previous_nums = results.find('span', attrs={'class': 'table_cell_block'})
+    print(previous_nums)
