bce95db51bf5ee14a70dbe53f67822d11785f628
Paul Bennett <pbennett.uk@outlook.com>
2019-01-03 15:26:25 +0000

    rm update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # luckylotto
 
 A simple 7 number generator to help pick winning lotto numbers. As the lucky dips are never lucky.
+
+`luckylotto.py` will generate 5 numbers between 1 & 50 via a for loop, it will then go on to use anothor for loop to create 2 numbers between 1 & 12 for the 2 needed lucky stars. Which then are both appended to a list to give the result as an example:
+
+Your luckylotto numbers could be [24, 17, 21, 35, 33, 12, 6]
--- luckylotto.py
+++ luckylotto.py
@@ -2,19 +2,19 @@ import random
 
 winning_numbers = []
 
-'''generate 5 numbers from 1 to 50 and 2 Lucky Stars from 1 to 12'''
-def generate_nums():
 
+def luckylotto():
     #  creates main lotto numbers
-    for num in range(5):
+    for ball_numbers in range(5):
         ball_numbers = random.randint(1, 50)
         winning_numbers.append(ball_numbers)
     
     #  creates lucky star numbers
-    for stars in range(2):
+    for lucky_star_nums in range(2):
         lucky_star_nums = random.randint(1, 12)
         winning_numbers.append(lucky_star_nums)
 
-    print(f'Your winning numbers could be {winning_numbers}')
+    print(f'Your luckylotto numbers could be {winning_numbers}')
 
-generate_nums()
+luckylotto()
+
