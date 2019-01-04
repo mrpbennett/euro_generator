199e6f0f17f26108cf8d4a0f4d26ef7ba6db8498
Paul Bennett <pbennett.uk@outlook.com>
2019-01-04 07:34:42 +0000

    generated ability to pull popular numbers in order, giving a user a choice.

--- euro_mill_scraper.py
+++ euro_mill_scraper.py
@@ -1,10 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 src = requests.get('https://www.national-lottery.co.uk/results/euromillions/draw-history').text
 
 soup = BeautifulSoup(src, 'lxml')
 
+csv_file = open('euromill_winners.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['winning numbers'])
+
 for results in soup.find_all('li', attrs={'class': 'table_cell_3'}):
     previous_nums = results.find('span', attrs={'class': 'table_cell_block'})
     print(previous_nums)
+
+    csv.writer.writerow([previous_nums])
+
+csv_file.close()
--- luckylotto.py
+++ luckylotto.py
@@ -1,45 +1,54 @@
 import random
+import operator
+import previous_winners as pw
 from collections import Counter
 
+
 winning_numbers = []
-previous_winners = [
-    [1, 8, 11, 25, 28, 4, 6],
-    [13, 16, 34, 35, 45, 10, 12],
-    [4, 5, 8, 31, 43, 2, 9],
-    [2, 12, 15, 34, 50, 3, 4],
-    [20, 25, 34, 42, 45, 6, 11],
-    [11, 17, 22, 23, 41, 6, 11],
-    [2, 4, 23, 39, 40, 3, 9],
-    [2, 15, 40, 43, 46, 3, 6],
-    [4, 6, 27, 48, 50, 1, 11],
-    [1, 4, 14, 21, 49, 2, 12],
-    [3, 8, 26, 33, 45, 7, 10],
-    [4, 30, 31, 38, 42, 4, 6],
-    [4, 16, 32, 42, 46, 8, 12],
-    [3, 14, 31, 36, 50, 1, 12]
-       
-]
 
 
-#  creates the 7 numbers you need for a euromillions lotto ticket
-def luckylotto():
-    #  creates main lotto numbers
-    for ball_numbers in range(5):
-        ball_numbers = random.randint(1, 50)
-        winning_numbers.append(ball_numbers)
-    
-    #  creates lucky star numbers
-    for lucky_star_nums in range(2):
-        lucky_star_nums = random.randint(1, 12)
-        winning_numbers.append(lucky_star_nums)
+def user_choice():
+    print('Please choose:')
+    print('1: If you want to see popular numbers from previous results')
+    print('2: To generate a luckydip and compare popularity agaisnt previous winners')
 
-    print(f'Your luckylotto numbers could be {winning_numbers}')
+    user_choice = input()
 
-    counts = Counter(n for l in previous_winners for n in l)
-    print('---')
-    print('generated number appears in previous winning numbers this many times: ')
-    print('---')
-    print({n: counts.get(n, 0) for n in winning_numbers})
+    if user_choice == '1':
+        #  generate and sorts previous_winners by the second element
+        #  to show most popular numbers drawn
+        pop_nums = Counter(n for l in pw.previous_winners for n in l)
+        ordered_pop_nums = sorted(pop_nums.items(), key=operator.itemgetter(1), reverse=True)
 
-luckylotto()
+        print('--- most popular numbers from previous results are ---')
+        print(ordered_pop_nums)
 
+    elif user_choice == '2':
+        #  creates main lotto numbers appends to winning_numbers
+        for ball_numbers in range(5):
+            ball_numbers = random.randint(1, 50)
+            winning_numbers.append(ball_numbers)
+
+        #  creates lucky star numbers and appends to winning_numbers
+        for lucky_star_nums in range(2):
+            lucky_star_nums = random.randint(1, 12)
+            winning_numbers.append(lucky_star_nums)
+
+        print('--- # ---')
+        print(f'Your luckylotto numbers could be {winning_numbers}')
+        print()
+        print('The numbers appear in previous winning numbers this many times:')
+        print()
+
+        #  flattens winning_numbers and counts how many times each number appears win previous_winners
+        counts = Counter(n for l in pw.previous_winners for n in l)
+        winners_count = {n: counts.get(n, 0) for n in winning_numbers}
+        ordered_winners_count = sorted(winners_count.items(), key=operator.itemgetter(1), reverse=True)
+        print(ordered_winners_count)
+        
+
+user_choice()
+
+'''
+(1, 16), (2, 15), (3, 20), (4, 19), (5, 9), (6, 12), (7, 10), (8, 15), (9, 12), (10, 9), (11, 14), (12, 21), (13, 4), (14, 5), (15, 7), (16, 6), (17, 7), (18, 7), (19, 2), (20, 4), (21, 5), (22, 1), (23, 7), (24, 3), (25, 5), (26, 6), (27, 4), (28, 5), (29, 4), (30, 5), (31, 7), (32, 6), (33, 2), (34, 3), (35, 3), (36, 6), (37, 6), (38, 5), (39, 3), (40, 5), (41, 2), (42, 9), (43, 9), (44, 5), (45, 9), (46, 3), (47, 2), (48, 5), (49, 3), (50, 5)
+'''
